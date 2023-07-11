ef19017b9d46ed5ffcaf8496d031dfeb263f4cc8
MartaSch <martaschab27@gmail.com>
2023-07-11 13:17:25 +0200

    hw-07

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import ContactForm from './ContactsForm/ContactsForm';
+import ContactList from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import Filter from './Filter/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectError, selectIsLoading } from 'redux/contacts/ContactsSelector';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/Operation';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <ContactForm />
+      {isLoading && !error && <p>Request in progress...</p>}
+      <Filter />
+      <ContactList key={nanoid()} />
     </div>
   );
-};
+}
